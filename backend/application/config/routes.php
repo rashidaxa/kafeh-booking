99f6af2501ea5f651bdd9bc125fdb4cf78213a08
rashidaxa <rashid.shahzad@axabiztech.com>
2026-09-06 15:05:10 +0500

    udpated project from last required.

--- backend/application/config/routes.php
+++ backend/application/config/routes.php
@@ -51,6 +51,6 @@ require_once(APPPATH.'config/routes_admin.php');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'admin';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
